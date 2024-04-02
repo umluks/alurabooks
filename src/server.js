2acd87a981ea1b3a5fd569c29dd544845dbf34ab
umluks <lucas.sga@gmail.com>
2024-04-02 18:30:10 -0300

    vai

--- src/server.js
+++ src/server.js
@@ -18,7 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Lucas Santiago")
-  console.log("Alurabooks disponível em http://localhost:8000")
+  console.log("Lucas dos Anjos")
   console.log("AllBooks disponível em http://localhost:8000")
 })
